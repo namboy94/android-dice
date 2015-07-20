0f8190e57d6b0ddc34f580f4092f54a694df60ad
Hermann Krumrey <hermann@krumreyh.com>
2015-07-21 01:34:20 +0200

    Outsourced the code for the dice roll to SingleDice.

--- android-dice/src/main/java/com/namibsun/android/dice/Main.java
+++ android-dice/src/main/java/com/namibsun/android/dice/Main.java
@@ -39,6 +39,7 @@ public class Main extends ActionBarActivity {
      * @param menu -
      * @return
      */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -67,79 +68,7 @@ public class Main extends ActionBarActivity {
     }
 
     public void rollDice(View view) {
-        this.image.setEnabled(false);
         this.vibrator.vibrate(2000);
-
-        new CountDownTimer(2000, 5) {
-            public void onTick(long millisUntilFinished) {
-                rotateImage(Main.this.image);
-                if (Main.this.animationCount == 10) {
-                    Main.this.image.setImageResource(getRandomImage());
-                    Main.this.animationCount = 0;
-                } else {
-                    Main.this.animationCount++;
-                }
-            }
-
-            public void onFinish() {
-                straightenImage(Main.this.image);
-                Main.this.image.setEnabled(true);
-            }
-        }.start();
-    }
-
-    /**
-     * Method to get a random dice image
-     * @return
-     */
-    private int getRandomImage() {
-        int random = (int) (Math.random() * 6 + 1);
-        System.out.print(random);
-        switch(random) {
-            case 1: return R.drawable.diceone500;
-            case 2: return R.drawable.dicetwo500;
-            case 3: return R.drawable.dicethree500;
-            case 4: return R.drawable.dicefour500;
-            case 5: return R.drawable.dicefive500;
-            case 6: return R.drawable.dicesix500;
-            default: return R.drawable.diceone500;
-        }
-    }
-
-    private int getSemiRandomImage(int last) {
-        int random = getRandomImage();
-        while(random == last) {
-            random = getRandomImage();
-        }
-        return random;
-    }
-
-    private void rotateImage(ImageView image) {
-
-        float rotation = image.getRotation();
-        if (rotation == 0.0f) {
-            image.setRotation(1.1f);
-        } else if (rotation > 0.0f && rotation < 10.0f) {
-            if ((rotation - (int)rotation) < 0.5f) {
-                image.setRotation(rotation + 1.0f);
-            } else {
-                image.setRotation(rotation - 1.0f);
-            }
-        } else if (rotation >= 10.0f) {
-            image.setRotation(9.9f);
-        } else if (rotation < 0.0f && rotation > -10.0f) {
-            if (((-1.0f * rotation) + (int)rotation) < 0.5f) {
-                image.setRotation(rotation - 1.0f);
-            } else {
-                image.setRotation(rotation + 1.0f);
-            }
-        } else if (rotation <= -10.0f) {
-            image.setRotation(-9.9f);
-        }
-
-    }
-
-    private void straightenImage(ImageView image) {
-        image.setRotation(0);
+        new SingleDice(this.image).rollDice();
     }
 }
