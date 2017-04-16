c43a317f460ae817de67ed9733614cc6b75735aa
Hermann Krumrey <hermann@krumreyh.com>
2017-04-16 18:31:55 +0200

    Implement basic Lottery Activity

---
+++ app/src/main/java/net/namibsun/dice/activities/LotteryActivity.kt
@@ -0,0 +1,40 @@
+package net.namibsun.dice.activities
+
+import android.os.Bundle
+import android.widget.TextView
+import net.namibsun.dice.R
+import net.namibsun.dice.helpers.initializeBottomMenuBar
+import net.namibsun.dice.helpers.initializeSettingsButton
+import net.namibsun.dice.objects.TextDie
+import net.namibsun.dice.objects.loadTheme
+
+class LotteryActivity : BaseActivity() {
+
+    val lotteryNumbers: MutableList<TextDie> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.setContentView(R.layout.lottery)
+        initializeBottomMenuBar(this)
+        initializeSettingsButton(this)
+
+        listOf(R.id.lottery_die_one, R.id.lottery_die_two, R.id.lottery_die_three,
+                R.id.lottery_die_four, R.id.lottery_die_five, R.id.lottery_die_six)
+                .mapTo(lotteryNumbers) {
+            TextDie(this,
+                    this.findViewById(it) as TextView,
+                    loadTheme(this.prefs!!),
+                    initialValue=1, limit=49, minimum=1)
+                }
+
+        this.lotteryNumbers.map { die -> die.view.setOnClickListener {
+            this.lotteryNumbers.map(TextDie::roll)
+        }}
+    }
+
+    override fun onResume() {
+        super.onResume()
+        this.lotteryNumbers.map { die -> die.updateTheme(loadTheme(this.prefs!!)) }
+    }
+
+}
--- app/src/main/java/net/namibsun/dice/helpers/CommonUiOperations.kt
+++ app/src/main/java/net/namibsun/dice/helpers/CommonUiOperations.kt
@@ -25,11 +25,7 @@ package net.namibsun.dice.helpers
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import net.namibsun.dice.R
-import net.namibsun.dice.activities.SettingsActivity
-import net.namibsun.dice.activities.MainActivity
-import net.namibsun.dice.activities.TextDieActivity
-import net.namibsun.dice.activities.TwoDiceActivity
-import net.namibsun.dice.activities.RiskDiceActivity
+import net.namibsun.dice.activities.*
 
 /**
  * Initializes the OnClickListener of the Settings button
@@ -58,5 +54,8 @@ fun initializeBottomMenuBar(context: AppCompatActivity) {
     }
     context.findViewById(R.id.text_dice_activity).setOnClickListener {
         context.startActivity(Intent(context, TextDieActivity::class.java))
+    }
+    context.findViewById(R.id.lottery_activity).setOnClickListener {
+        context.startActivity(Intent(context, LotteryActivity::class.java))
     }
 }
