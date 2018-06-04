a2c8bdcc1b1a7e05fd74f5480cfcfbe829295de2
Hermann Krumrey <hermann@krumreyh.com>
2018-06-04 15:13:07 +0200

    Integrate ci-scrips

--- app/src/main/java/net/namibsun/dice/helpers/CommonUiOperations.kt
+++ app/src/main/java/net/namibsun/dice/helpers/CommonUiOperations.kt
@@ -1,20 +1,23 @@
 /*
-Copyright 2015-2018 Hermann Krumrey<hermann@krumreyh.com>
+Copyright 2015 Hermann Krumrey
 
-This file is part of android-dice.
+This file is part of dice-roller.
 
-android-dice is free software: you can redistribute it and/or modify
+dice-roller is an Android app that allows a user to roll a virtual
+die. Multiple configurations are supported
+
+dice-roller is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-android-dice is distributed in the hope that it will be useful,
+dice-roller is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with android-dice.  If not, see <http://www.gnu.org/licenses/>.
+along with dice-roller. If not, see <http://www.gnu.org/licenses/>.
 */
 
 package net.namibsun.dice.helpers
